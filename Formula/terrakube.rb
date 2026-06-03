f5f6324c5d189fea8bb9f7cf4078137a5a510687
Alfredo España <alfespa17@gmail.com>
2026-06-03 15:15:06 -0600

    Add GitHub Actions workflow to build and upload Homebrew bottles (#15)

--- Formula/terrakube.rb
+++ Formula/terrakube.rb
@@ -24,9 +24,12 @@ class Terrakube < Formula
       sha256 "525b8a69b6c1d8c32dcb24d18ba0783a57b3d637339a6bcb7b39de05b892576e"
     end
 
-    # Homebrew for Linux requires a compiler to be present to install any formula 
-    # from source (i.e. without a bottle), even if it's a pre-compiled binary.
-    depends_on "gcc" => :build
+  bottle do
+    root_url "https://github.com/terrakube-io/homebrew-cli/releases/download/v1.0.0-beta.5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "56b67362e10b25ba5368096938c87d408c2a3d9c484358e9cfef1eb2ad68b11a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6be8bf72b8f00ef159d21062f403c58da49238d9e3cc2e9b2df24f19832dd1ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "739f4b134d00942064dde39bc6bb23b009ffeb0313f61f1c24eaf96fb38e7aa4"
+    sha256 cellar: :any_skip_relocation, aarch64_linux:  "7bdb9ab62c6b5aa19c9ee9308ecb56b967159dffbef6e214cb0523a3cbe3b7ad"
   end
 
   def install
@@ -34,6 +37,6 @@ class Terrakube < Formula
   end
 
   test do
-    system "#{bin}/terrakube", "--version"
+    system "#{bin}/terrakube", "help"
   end
 end
