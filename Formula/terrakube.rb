84de4573338c6d818c5f5c3d269e9662ae8b02e6
Alfredo España <alfespa17@gmail.com>
2026-06-03 11:46:30 -0600

    Refactor Terrakube formula for version handling

--- Formula/terrakube.rb
+++ Formula/terrakube.rb
@@ -1,14 +1,35 @@
 class Terrakube < Formula
-    desc "terrakube command line tool"
-    homepage "https://docs.terrakube.io/"
-    url "https://github.com/terrakube-io/terrakube-cli/releases/download/v1.0.0-beta.5/terrakube-v1.0.0-beta.5-darwin-amd64.tar.gz"
-    sha256 "1a071b793ddd86b68577857915fd800c9c795a299e1ce32693df116cbe95dd02"
-    license "Apache-2.0"
-  
-  
-    def install
-      bin.install 'terrakube'
+  desc "terrakube command line tool"
+  homepage "https://docs.terrakube.io/"
+  license "Apache-2.0"
+
+  version "1.0.0-beta.5"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/terrakube-io/terrakube-cli/releases/download/v#{version}/terrakube-v#{version}-darwin-amd64.tar.gz"
+      sha256 "1a071b793ddd86b68577857915fd800c9c795a299e1ce32693df116cbe95dd02"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/terrakube-io/terrakube-cli/releases/download/v#{version}/terrakube-v#{version}-darwin-arm64.tar.gz"
+      sha256 "650b35381b5fbc2111b574f3b39da89c68de6e9bf4a5eb97a7bcdba79e4d456c"
     end
-  
   end
-  
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/terrakube-io/terrakube-cli/releases/download/v#{version}/terrakube-v#{version}-linux-amd64.tar.gz"
+      sha256 "5a41c520a115f4e3155ae474688ce674207c9dabd597823be7f4524d7a66e536"
+    elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/terrakube-io/terrakube-cli/releases/download/v#{version}/terrakube-v#{version}-linux-arm64.tar.gz"
+      sha256 "525b8a69b6c1d8c32dcb24d18ba0783a57b3d637339a6bcb7b39de05b892576e"
+    end
+  end
+
+  def install
+    bin.install "terrakube"
+  end
+
+  test do
+    system "#{bin}/terrakube", "--version"
+  end
+end
